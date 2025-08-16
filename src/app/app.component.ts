31bee8e892344fb93ee273672cad4a55784201af
Rajesh <chintarajesh456@gmail.com>
2025-08-16 13:30:45 +0530

    home component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./Header/header/header.component";
-import { ProductComponent } from "./Product/product/product.component";
-import { AboutUsComponent } from "./About_Us/about-us/about-us.component";
-import { ContactUsComponent } from "./Contact_Us/contact-us/contact-us.component";
 import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, 
-    ProductComponent, 
-    AboutUsComponent, 
-    ContactUsComponent, 
+  imports: [HeaderComponent,  
     RouterOutlet,
     MatIconModule
     ],
