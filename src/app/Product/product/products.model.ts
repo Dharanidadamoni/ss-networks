254d76b38732bf6e98227923a45a836db183984f
Rajesh <chintarajesh456@gmail.com>
2025-08-16 16:19:42 +0530

    changes related to home page

--- src/app/Product/product/products.model.ts
+++ src/app/Product/product/products.model.ts
@@ -1,47 +1,57 @@
 export const products = [
     {
-        imgPath:"https://cdn.pixabay.com/photo/2022/11/27/17/54/soccer-7620418_1280.jpg",
-        heading:"Standard Balcony Net",
-        description:"Perfect for balconies and small open spaces. Made from UV-stabilized polyethylene.",
+        category:"Balcony Nets",
+        categoryList:[
+            {
+                imgPath:"https://ehomes.com.sg/wp-content/uploads/2022/07/Screenshot-2022-08-15-at-3.29.26-AM.png",
+                heading:"Standard Balcony Net",
+                description:"Perfect for balconies and small open spaces. Made from UV-stabilized polyethylene.",
+            },
+            {
+                imgPath:"https://cdn.pixabay.com/photo/2023/08/28/23/17/superb-fairywren-8220199_1280.jpg",
+                heading:"Window Safety Net",
+                description:"Custom-fit nets for windows of all sizes. Provides maximum protection without blocking the view.",
+            },
+            {
+                imgPath:"https://ehomes.com.sg/wp-content/uploads/2022/07/Screenshot-2022-08-15-at-3.29.26-AM.png",
+                heading:"Standard Balcony Net",
+                description:"Perfect for balconies and small open spaces. Made from UV-stabilized polyethylene.",
+            },
+            {
+                imgPath:"https://cdn.pixabay.com/photo/2023/08/28/23/17/superb-fairywren-8220199_1280.jpg",
+                heading:"Window Safety Net",
+                description:"Custom-fit nets for windows of all sizes. Provides maximum protection without blocking the view.",
+            },
+        ]
     },
     {
+        category:"Garden Nets",
+        categoryList:[
+        {
+            
+            imgPath:"https://cdn.pixabay.com/photo/2021/10/30/18/03/bridge-6755247_1280.jpg",
+            heading:"Terrace & Garden Net",
+            description:"A heavy-duty solution for larger areas like terraces and gardens.",
+        },
+        {
+            imgPath:"https://cdn.pixabay.com/photo/2022/11/27/17/54/soccer-7620418_1280.jpg",
+            heading:"Standard Balcony Net",
+            description:"Perfect for balconies and small open spaces. Made from UV-stabilized polyethylene.",
+        },
+        
+        ]
+    },
+    {
+        category:"Sports Nets",
+        categoryList:[{
         imgPath:"https://cdn.pixabay.com/photo/2023/08/28/23/17/superb-fairywren-8220199_1280.jpg",
         heading:"Window Safety Net",
         description:"Custom-fit nets for windows of all sizes. Provides maximum protection without blocking the view.",
-    },
-    {
-        imgPath:"https://cdn.pixabay.com/photo/2021/10/30/18/03/bridge-6755247_1280.jpg",
-        heading:"Terrace & Garden Net",
-        description:"A heavy-duty solution for larger areas like terraces and gardens.",
-    },
-    {
-        imgPath:"https://cdn.pixabay.com/photo/2022/11/27/17/54/soccer-7620418_1280.jpg",
-        heading:"Standard Balcony Net",
-        description:"Perfect for balconies and small open spaces. Made from UV-stabilized polyethylene.",
-    },
-    {
-        imgPath:"https://cdn.pixabay.com/photo/2023/08/28/23/17/superb-fairywren-8220199_1280.jpg",
-        heading:"Window Safety Net",
-        description:"Custom-fit nets for windows of all sizes. Provides maximum protection without blocking the view.",
-    },
-    {
-        imgPath:"https://cdn.pixabay.com/photo/2021/10/30/18/03/bridge-6755247_1280.jpg",
-        heading:"Terrace & Garden Net",
-        description:"A heavy-duty solution for larger areas like terraces and gardens.",
-    },
-    {
-        imgPath:"https://cdn.pixabay.com/photo/2022/11/27/17/54/soccer-7620418_1280.jpg",
-        heading:"Standard Balcony Net",
-        description:"Perfect for balconies and small open spaces. Made from UV-stabilized polyethylene.",
-    },
-    {
-        imgPath:"https://cdn.pixabay.com/photo/2023/08/28/23/17/superb-fairywren-8220199_1280.jpg",
-        heading:"Window Safety Net",
-        description:"Custom-fit nets for windows of all sizes. Provides maximum protection without blocking the view.",
-    },
-    {
-        imgPath:"https://cdn.pixabay.com/photo/2021/10/30/18/03/bridge-6755247_1280.jpg",
-        heading:"Terrace & Garden Net",
-        description:"A heavy-duty solution for larger areas like terraces and gardens.",
-    },
+        },
+        {
+            imgPath:"https://cdn.pixabay.com/photo/2021/10/30/18/03/bridge-6755247_1280.jpg",
+            heading:"Terrace & Garden Net",
+            description:"A heavy-duty solution for larger areas like terraces and gardens.",
+        }]
+    }
 ]
