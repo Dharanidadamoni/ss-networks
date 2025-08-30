3e3600358969c72f5a19200816904bd6d9aee880
Rajesh <chintarajesh456@gmail.com>
2025-08-30 18:28:34 +0530

    na

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { ProductComponent } from './Product/product/product.component';
+import { ProductComponent } from './components/product/product.component';
 import { HeaderComponent } from './Header/header/header.component';
 import { AboutUsComponent } from './About_Us/about-us/about-us.component';
-import { ContactUsComponent } from './Contact_Us/contact-us/contact-us.component';
-import { HomeComponentComponent } from './Home_component/home-component/home-component.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponentComponent }, // Route for the home page
